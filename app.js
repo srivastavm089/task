9e88b68adc97cdf1c32db9cfb64072e27bd0c8a5
Abhay Kumar Srivastava <ms2364911@gmail.com>
2023-12-24 19:15:54 +0530

    addes something

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 import userRoute from "./routes/userRoute.js"
 const app = express();
-app.use(express.json({limit:"50mb"}));
 app.use(cors());
+app.use(express.json({limit:"50mb"}));
+app.use(express.urlencoded({ limit: "500mb", extended: true }));
+
 app.use("/task/v1",userRoute)
 
 export default app;
